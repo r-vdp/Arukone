8cc0c11361f6124e328a30ca02d28e03a8090d23
RamsesDeNorre <ramses.rommel@gmail.com>
2012-10-29 20:25:19 +0100

    Use functor/arg to get constant time member checks.

--- oplossing.pl
+++ oplossing.pl
@@ -26,28 +26,23 @@ solve(PuzzleId, Solution) :-
 % Cut at the end after finding a path because we could assume that every puzzle
 % has a unique solution. Backtracking is thus guaranteed to be useless.
 arukone(Grid, Links, Solution) :-
-    to_occupied(Links, Occupied),
+    init_occupied(Links, Grid, Occupied),
     order_links(Links, OrderedLinks),
     find_paths(Grid, OrderedLinks, Occupied, Solution),
     !.
 
-% to_occupied(Links, Occupied) succeeds if Occupied is a list of pos terms
-% consisting of the two positions in each link term that is contained in Links.
-to_occupied([], []).
-to_occupied([link(_, Pos1, Pos2) | Links], [Pos1, Pos2 | Tail]) :-
-    to_occupied(Links, Tail).
-
 find_paths(_, [], _, []).
 find_paths(Grid, [link(Type, From, To)|Links], Occupied, [connects(Type, Path)|Paths]) :-
-    find_path(Grid, From, To, Occupied, Path, NewOccupied),
-    find_paths(Grid, Links, NewOccupied, Paths).
+    find_path(Grid, From, To, Occupied, Path),
+    find_paths(Grid, Links, Occupied, Paths).
 
-find_path(Grid, From, To, Occupied, [From, To], Occupied) :-
+find_path(Grid, From, To, _, [From, To]) :-
     neighbours(From, To, Grid).
-find_path(Grid, From, To, Occupied, [From|SubPath], NewOccupied) :-
+find_path(Grid, From, To, Occupied, [From|SubPath]) :-
     \+ neighbours(From, To, Grid),
     best_neighbour(From, To, Grid, Occupied, Node),
-    find_path(Grid, Node, To, [Node|Occupied], SubPath, NewOccupied).
+    set_occupied(Node, Grid, Occupied),
+    find_path(Grid, Node, To, Occupied, SubPath).
 
 best_neighbour(From, To, Grid, Occupied, Neighbour) :-
     sorted_neighbours(From, To, Grid, Occupied, Neighbours),
@@ -55,9 +50,34 @@ best_neighbour(From, To, Grid, Occupied, Neighbour) :-
 
 sorted_neighbours(From, To, Grid, Occupied, Neighbours) :-
     findall((From, To, Grid, Neighbour),
-            (neighbours(From, Neighbour, Grid), \+ member(Neighbour, Occupied)),
+            (neighbours(From, Neighbour, Grid), \+ is_occupied(Neighbour, Grid, Occupied)),
             UnsortedNeighbours),
-    predsort(current_ordering, UnsortedNeighbours, Neighbours).
+    order_neighbours(UnsortedNeighbours, Neighbours).
+
+
+init_occupied(Links, Grid, Occupied) :-
+    create_occupied(Grid, Occupied),
+    fill_occupied(Links, Grid, Occupied).
+
+create_occupied(grid(N, M), Occupied) :-
+    Size is N * M,
+    functor(Occupied, occupied, Size).
+
+fill_occupied([], _, _).
+fill_occupied([link(_, First, Second) | Links], Grid, Occupied) :-
+    set_occupied(First, Grid, Occupied),
+    set_occupied(Second, Grid, Occupied),
+    fill_occupied(Links, Grid, Occupied).
+
+set_occupied(pos(R, C), grid(N, _), Occupied) :-
+    Idx is ((R - 1) * N) + C,
+    arg(Idx, Occupied, true).
+
+is_occupied(pos(R, C), grid(_, N), Occupied) :-
+    Idx is ((R - 1) * N) + C,
+    arg(Idx, Occupied, Result),
+    Result == true.
+
 
 neighbours(pos(A, B), pos(C, B), _) :-
     A > 1,
@@ -74,6 +94,53 @@ neighbours(pos(A, B), pos(A, C), grid(_, N)) :-
 
 
 %%%%%%%%% Orderings %%%%%%%%%
+
+order_links(Links, OrderedLinks) :-
+    predsort(longest_paths_first, Links, OrderedLinks).
+
+longest_paths_first(Delta, link(_, pos(R1, C1), pos(R2, C2)), link(_, pos(R3, C3), pos(R4, C4))) :-
+    Diff1 is abs(R1 - R2) + abs(C1 - C2),
+    Diff2 is abs(R3 - R4) + abs(C3 - C4),
+    compare_no_equals(Delta, Diff2, Diff1).
+
+order_neighbours(Neighbours, OrderedNeighbours) :-
+    predsort(along_border_ordering, Neighbours, OrderedNeighbours).
+
+along_border_ordering(Delta, (From, To, Grid, _), (From, To, Grid, Second)) :-
+    ( on_border(Second, Grid) ->
+        Delta = >
+    ;
+        Delta = <
+    ).
+
+on_border(pos(0, _), _).
+on_border(pos(R, 0), _) :- 
+    R \== 0.
+on_border(pos(N, C), grid(N, _)) :-
+    C \== 0.
+on_border(pos(R, M), grid(N, M)):- 
+    R \== 0,
+    R \== N.
+   
+compare_no_equals(Delta, A, B) :-
+    ( A < B ->
+        Delta = <
+    ;
+        Delta = >
+    ).
+
+
+
+
+
+
+
+%%%%%%%%% Unused %%%%%%%%%%
+/*
+shortest_paths_first(Delta, link(_, pos(R1, C1), pos(R2, C2)), link(_, pos(R3, C3), pos(R4, C4))) :-
+    Diff1 is abs(R1 - R2) + abs(C1 - C2),
+    Diff2 is abs(R3 - R4) + abs(C3 - C4),
+    compare_no_equals(Delta, Diff1, Diff2).
 
 current_ordering(Delta, A, B) :-
     %orig_ordering(Delta, A, B).
@@ -117,29 +184,6 @@ towards_goal2(Delta, (From, To, _, pos(R1, C1)), (From, To, _, pos(R2, C2))) :-
     compare_no_equals(Delta, Length1, Length2).
 
 
-along_border_ordering(Delta, (From, To, Grid, _), (From, To, Grid, Second)) :-
-    ( on_border(Second, Grid) ->
-        Delta = >
-    ;
-        Delta = <
-    ).
-
-on_border(pos(0, _), _).
-on_border(pos(R, 0), _) :- 
-    R \== 0.
-on_border(pos(N, C), grid(N, _)) :-
-    C \== 0.
-on_border(pos(R, M), grid(N, M)):- 
-    R \== 0,
-    R \== N.
-   
-compare_no_equals(Delta, A, B) :-
-    ( A < B ->
-        Delta = <
-    ;
-        Delta = >
-    ).
-
 random_ordering(Delta, _, _) :-
     ( 0 =:= random(2) ->
         Delta = <
@@ -158,17 +202,5 @@ combined(Delta, Node1, Node2) :-
     \+ on_border(First, Grid),
     \+ on_border(Second, Grid),
     towards_goal_ordering(Delta, Node1, Node2).
-
-order_links(Links, OrderedLinks) :-
-    predsort(longest_paths_first, Links, OrderedLinks).
-
-shortest_paths_first(Delta, link(_, pos(R1, C1), pos(R2, C2)), link(_, pos(R3, C3), pos(R4, C4))) :-
-    Diff1 is abs(R1 - R2) + abs(C1 - C2),
-    Diff2 is abs(R3 - R4) + abs(C3 - C4),
-    compare_no_equals(Delta, Diff1, Diff2).
-
-longest_paths_first(Delta, link(_, pos(R1, C1), pos(R2, C2)), link(_, pos(R3, C3), pos(R4, C4))) :-
-    Diff1 is abs(R1 - R2) + abs(C1 - C2),
-    Diff2 is abs(R3 - R4) + abs(C3 - C4),
-    compare_no_equals(Delta, Diff2, Diff1).
+*/
 
