688c194dfe889b1afbc4ffc17ac6906974693f74
RamsesDeNorre <ramses.rommel@gmail.com>
2012-10-29 20:28:51 +0100

    Use difference lists for linear member checks.

--- oplossing.pl
+++ oplossing.pl
@@ -26,7 +26,7 @@ solve(PuzzleId, Solution) :-
 % Cut at the end after finding a path because we could assume that every puzzle
 % has a unique solution. Backtracking is thus guaranteed to be useless.
 arukone(Grid, Links, Solution) :-
-    to_occupied(Links, Occupied),
+    init_occupied(Links, Occupied),
     order_links(Links, OrderedLinks),
     find_paths(Grid, OrderedLinks, Occupied, Solution),
     !.
@@ -47,7 +47,8 @@ find_path(Grid, From, To, Occupied, [From, To], Occupied) :-
 find_path(Grid, From, To, Occupied, [From|SubPath], NewOccupied) :-
     \+ neighbours(From, To, Grid),
     best_neighbour(From, To, Grid, Occupied, Node),
-    find_path(Grid, Node, To, [Node|Occupied], SubPath, NewOccupied).
+    set_occupied(Node, Occupied, Occupied2),
+    find_path(Grid, Node, To, Occupied2, SubPath, NewOccupied).
 
 best_neighbour(From, To, Grid, Occupied, Neighbour) :-
     sorted_neighbours(From, To, Grid, Occupied, Neighbours),
@@ -55,9 +56,9 @@ best_neighbour(From, To, Grid, Occupied, Neighbour) :-
 
 sorted_neighbours(From, To, Grid, Occupied, Neighbours) :-
     findall((From, To, Grid, Neighbour),
-            (neighbours(From, Neighbour, Grid), \+ member(Neighbour, Occupied)),
+            (neighbours(From, Neighbour, Grid), \+ is_occupied(Neighbour, Occupied)),
             UnsortedNeighbours),
-    predsort(current_ordering, UnsortedNeighbours, Neighbours).
+    order_neighbours(UnsortedNeighbours, Neighbours).
 
 neighbours(pos(A, B), pos(C, B), _) :-
     A > 1,
@@ -73,49 +74,50 @@ neighbours(pos(A, B), pos(A, C), grid(_, N)) :-
     C is B + 1.
 
 
+
+init_occupied(Links, Occupied) :-
+    fill_occupied(Links, [], Occupied).
+
+fill_occupied([], Occupied, Occupied).
+fill_occupied([link(_, First, Second) | Links], Occupied, NewOccupied) :-
+    set_occupied(First, Occupied, Occupied2),
+    set_occupied(Second, Occupied2, Occupied3),
+    fill_occupied(Links, Occupied3, NewOccupied).
+
+is_occupied(pos(R, C), [(Row, Columns) | Tail]) :-
+    ( R == Row ->
+        memberchk(C, Columns)
+    ;
+        is_occupied(pos(R, C), Tail)
+    ).
+
+set_occupied(Pos, Occupied, NewOccupied) :-
+    set_occupied(Pos, Occupied-[], A-A, NewOccupied).
+
+set_occupied(pos(R, C), A-A, Acc-ATail, NewOccupied) :-
+    [(R, [C])|Acc]-ATail = NewOccupied-[].
+set_occupied(pos(R, C), [(Row, Columns)|Occupied]-OTail, Acc-ATail, NewOccupied) :-
+    ( R == Row ->
+        NewColumns = [C|Columns],
+        ATail = [(R, NewColumns)|Occupied],
+        Acc-OTail = NewOccupied-[]
+    ;
+        set_occupied(pos(R, C), Occupied-OTail, [(Row, Columns)|Acc]-ATail, NewOccupied)
+    ).
+
+
 %%%%%%%%% Orderings %%%%%%%%%
 
-current_ordering(Delta, A, B) :-
-    %orig_ordering(Delta, A, B).
-    along_border_ordering(Delta, A, B).
-    %random_ordering(Delta, A, B).
-    %towards_goal_ordering(Delta, A, B).
-    %towards_goal2(Delta, A, B).
-    %combined(Delta, A, B).
-    
+order_links(Links, OrderedLinks) :-
+    predsort(longest_paths_first, Links, OrderedLinks).
 
-orig_ordering(<, _, _).
+longest_paths_first(Delta, link(_, pos(R1, C1), pos(R2, C2)), link(_, pos(R3, C3), pos(R4, C4))) :-
+    Diff1 is abs(R1 - R2) + abs(C1 - C2),
+    Diff2 is abs(R3 - R4) + abs(C3 - C4),
+    compare_no_equals(Delta, Diff2, Diff1).
 
-% towards_goal_ordering(Delta, (From, To, Grid, Node1), (From, To, Grid, Node2))
-% is an ordering key to be used with predsort, it returns < if Node1 is closer
-% to To than Node2.
-towards_goal_ordering(Delta, (_, Goal, _, pos(R1, C1)), (_, Goal, _, pos(R2, C2))) :-
-    Goal = pos(RG, CG),
-    Diff1 = abs(RG - R1) + abs(CG - C1),
-    Diff2 = abs(RG - R2) + abs(CG - C2),
-    compare_no_equals(Delta, Diff1, Diff2).
-
-towards_goal2(Delta, (From, To, _, pos(_, C1)), (From, To, _, pos(_, C2))) :-
-    From = pos(R, _),
-    To = pos(R, C),
-    Length1 is abs(C - C1),
-    Length2 is abs(C - C2),
-    compare_no_equals(Delta, Length1, Length2).
-towards_goal2(Delta, (From, To, _, pos(R1, _)), (From, To, _, pos(R2, _))) :-
-    From = pos(_, C),
-    To = pos(R, C),
-    Length1 is abs(R - R1),
-    Length2 is abs(R - R2),
-    compare_no_equals(Delta, Length1, Length2).
-towards_goal2(Delta, (From, To, _, pos(R1, C1)), (From, To, _, pos(R2, C2))) :-
-    From = pos(FromR, FromC),
-    To = pos(R, C),
-    R =\= FromR,
-    C =\= FromC,
-    Length1 is abs(R - R1) + abs(C - C1),
-    Length2 is abs(R - R2) + abs(C - C2),
-    compare_no_equals(Delta, Length1, Length2).
-
+order_neighbours(Neighbours, OrderedNeighbours) :-
+    predsort(along_border_ordering, Neighbours, OrderedNeighbours).
 
 along_border_ordering(Delta, (From, To, Grid, _), (From, To, Grid, Second)) :-
     ( on_border(Second, Grid) ->
@@ -132,43 +134,11 @@ on_border(pos(N, C), grid(N, _)) :-
 on_border(pos(R, M), grid(N, M)):- 
     R \== 0,
     R \== N.
-   
+
 compare_no_equals(Delta, A, B) :-
     ( A < B ->
         Delta = <
     ;
         Delta = >
     ).
-
-random_ordering(Delta, _, _) :-
-    ( 0 =:= random(2) ->
-        Delta = <
-    ;
-        Delta = >
-    ).
-
-combined(<, (_, _, Grid, First), _) :-
-    on_border(First, Grid).
-combined(>, (From, To, Grid, First), (From, To, Grid, Second)) :-
-    \+ on_border(First, Grid),
-    on_border(Second, Grid).
-combined(Delta, Node1, Node2) :-
-    Node1 = (_, _, Grid, First),
-    Node2 = (_, _, Grid, Second),
-    \+ on_border(First, Grid),
-    \+ on_border(Second, Grid),
-    towards_goal_ordering(Delta, Node1, Node2).
-
-order_links(Links, OrderedLinks) :-
-    predsort(longest_paths_first, Links, OrderedLinks).
-
-shortest_paths_first(Delta, link(_, pos(R1, C1), pos(R2, C2)), link(_, pos(R3, C3), pos(R4, C4))) :-
-    Diff1 is abs(R1 - R2) + abs(C1 - C2),
-    Diff2 is abs(R3 - R4) + abs(C3 - C4),
-    compare_no_equals(Delta, Diff1, Diff2).
-
-longest_paths_first(Delta, link(_, pos(R1, C1), pos(R2, C2)), link(_, pos(R3, C3), pos(R4, C4))) :-
-    Diff1 is abs(R1 - R2) + abs(C1 - C2),
-    Diff2 is abs(R3 - R4) + abs(C3 - C4),
-    compare_no_equals(Delta, Diff2, Diff1).
 
