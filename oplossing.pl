040aac86efa8095be659839e8a232de9762eaa65
RamsesDeNorre <ramses.rommel@gmail.com>
2012-11-04 12:47:04 +0100

    Playing around with the heuristics...

--- oplossing.pl
+++ oplossing.pl
@@ -40,17 +40,17 @@ find_path(Grid, From, To, Occupied, [From, To], Occupied) :-
     neighbours(From, To, Grid).
 find_path(Grid, From, To, Occupied, [From|SubPath], NewOccupied) :-
     \+ neighbours(From, To, Grid),
-    best_neighbour(From, To, Grid, Occupied, Node),
+    best_neighbour(From, Grid, Occupied, Node),
     set_occupied(Node, Occupied, Occupied2),
     find_path(Grid, Node, To, Occupied2, SubPath, NewOccupied).
 
 
-best_neighbour(From, To, Grid, Occupied, Neighbour) :-
-    sorted_neighbours(From, To, Grid, Occupied, Neighbours),
-    member((From, To, Grid, Neighbour), Neighbours).
+best_neighbour(From, Grid, Occupied, Neighbour) :-
+    sorted_neighbours(From, Grid, Occupied, Neighbours),
+    member((Grid, Neighbour), Neighbours).
 
-sorted_neighbours(From, To, Grid, Occupied, Neighbours) :-
-    findall((From, To, Grid, Neighbour),
+sorted_neighbours(From, Grid, Occupied, Neighbours) :-
+    findall((Grid, Neighbour),
             (neighbours(From, Neighbour, Grid), \+ is_occupied(Neighbour, Occupied)),
             UnsortedNeighbours),
     order_neighbours(UnsortedNeighbours, Neighbours).
@@ -109,36 +109,48 @@ order_links(Links, Grid, OrderedLinks) :-
     findall(Link, member((Link, Grid), TmpOrderedLinks), OrderedLinks).
 
 link_ordering(Delta, Tuple1, Tuple2) :-
-    combine(on_border_link_ordering, longest_paths_first, Delta, Tuple1, Tuple2).
+    Preds = [all_on_border_link_ordering, longest_paths_first],
+    combine(Preds, Tuple1, Tuple2, Delta).
 
-on_border_link_ordering(<, (Link, Grid), _) :-
-    on_border_link(Link, Grid).
-on_border_link_ordering(>, (Link1, Grid), (Link2, Grid)) :-
-    \+ on_border_link(Link1, Grid),
-    on_border_link(Link2, Grid).
-on_border_link_ordering(=, (Link1, Grid), (Link2, Grid)) :-
-    \+ on_border_link(Link1, Grid),
-    \+ on_border_link(Link2, Grid).
+all_on_border_link_ordering(<, (Link, Grid), _) :-
+    all_on_border(Link, Grid).
+all_on_border_link_ordering(>, (Link1, Grid), (Link2, Grid)) :-
+    \+ all_on_border(Link1, Grid),
+    all_on_border(Link2, Grid).
+all_on_border_link_ordering(=, (Link1, Grid), (Link2, Grid)) :-
+    \+ all_on_border(Link1, Grid),
+    \+ all_on_border(Link2, Grid).
 
-longest_paths_first(Delta, (link(_, pos(R1, C1), pos(R2, C2)), _), (link(_, pos(R3, C3), pos(R4, C4)), _)) :-
+
+any_on_border_link_ordering(<, (Link, Grid), _) :-
+    any_on_border(Link, Grid).
+any_on_border_link_ordering(>, (Link1, Grid), (Link2, Grid)) :-
+    \+ any_on_border(Link1, Grid),
+    any_on_border(Link2, Grid).
+any_on_border_link_ordering(=, (Link1, Grid), (Link2, Grid)) :-
+    \+ any_on_border(Link1, Grid),
+    \+ any_on_border(Link2, Grid).
+
+longest_paths_first(Delta,
+                    (link(_, pos(R1, C1), pos(R2, C2)), _),
+                    (link(_, pos(R3, C3), pos(R4, C4)), _)) :-
     Diff1 is abs(R1 - R2) + abs(C1 - C2),
     Diff2 is abs(R3 - R4) + abs(C3 - C4),
     compare_no_equals(Delta, Diff2, Diff1).
 
-combine(Pred1, Pred2, Delta, First, Second) :-
-    Term1 =.. [Pred1, D1, First, Second],
-    call(Term1),
-    ( D1 == (=) ->
-        Term2 =.. [Pred2, Delta, First, Second],
-        call(Term2)
+combine([Pred|Preds], First, Second, Delta) :-
+    Term =.. [Pred, NewDelta, First, Second],
+    call(Term),
+    ( NewDelta == (=) ->
+        combine(Preds, First, Second, Delta)
     ;
-        Delta = D1
+        Delta = NewDelta
     ).
 
 order_neighbours(Neighbours, OrderedNeighbours) :-
     predsort(along_border_ordering, Neighbours, OrderedNeighbours).
 
-along_border_ordering(Delta, (From, To, Grid, _), (From, To, Grid, Second)) :-
+along_border_ordering(Delta, (Grid, _), (Grid, Second)) :-
     ( on_border(Second, Grid) ->
         Delta = >
     ;
@@ -154,8 +166,14 @@ on_border(pos(R, M), grid(N, M)):-
     R =\= 1,
     R =\= N.
 
-on_border_link(link(_, From, To), Grid) :-
+all_on_border(link(_, From, To), Grid) :-
     on_border(From, Grid),
+    on_border(To, Grid).
+
+any_on_border(link(_, From, _), Grid) :-
+    on_border(From, Grid).
+any_on_border(link(_, From, To), Grid) :-
+    \+ on_border(From, Grid),
     on_border(To, Grid).
 
 compare_no_equals(Delta, A, B) :-
